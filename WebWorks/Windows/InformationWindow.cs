f5f2897fcaed56dd973653759acec5901126785b
okangel <okangel12345@gmail.com>
2025-01-31 16:05:12 -0600

    Add assembly version to info

--- WebWorks/Windows/InformationWindow.cs
+++ WebWorks/Windows/InformationWindow.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Reflection;
 
 namespace WebWorks.Windows
 {
@@ -24,6 +25,8 @@ namespace WebWorks.Windows
             MinimizeBox = false;
 
             richTextBox1.ReadOnly = true;
+
+            richTextBox1.AppendText($"v{Assembly.GetExecutingAssembly().GetName().Version.ToString(3)}");
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
